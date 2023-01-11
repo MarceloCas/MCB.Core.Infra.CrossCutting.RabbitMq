65cea54a23a5180cbec0aa039ec72d6f07471ae1
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2023-01-10 22:42:56 -0300

    feat: add ExchangeDeclare method

--- src/MCB.Core.Infra.CrossCutting.RabbitMq/Connection/Interfaces/IRabbitMqConnection.cs
+++ src/MCB.Core.Infra.CrossCutting.RabbitMq/Connection/Interfaces/IRabbitMqConnection.cs
@@ -8,16 +8,18 @@ public interface IRabbitMqConnection
     void OpenConnection(bool forceReopen = false);
 
     bool CheckIfExchangeExists(string exchangeName);
-    void PublishExchange(RabbitMqExchangeConfig exchangeConfig, string routingKey, IBasicProperties properties, ReadOnlyMemory<byte> message);
-
+    void ExchangeDeclare(RabbitMqExchangeConfig exchangeConfig);
+    
     bool CheckIfQueueExists(string queueName);
     QueueDeclareOk? QueueDeclare(RabbitMqQueueConfig queueConfig);
-    void PublishQueue(RabbitMqQueueConfig queueConfig, IBasicProperties properties, ReadOnlyMemory<byte> message);
     void PurgeQueue(string queueName);
     bool DeleteQueue(string queueName, bool ifUnused = false, bool ifEmpty = false);
     (uint messageCount, uint consumerCount)? GetQueueCounters(string queueName);
 
     void DisconectConsumer(string consumerTag);
+
+    void PublishExchange(RabbitMqExchangeConfig exchangeConfig, string routingKey, IBasicProperties properties, ReadOnlyMemory<byte> message);
+    void PublishQueue(RabbitMqQueueConfig queueConfig, IBasicProperties properties, ReadOnlyMemory<byte> message);
 
     IBasicProperties CreateBasicProperties();
 }
