92f95abdca75dc0c8919ecdbd660a3f5fa646ef9
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2023-01-10 22:34:36 -0300

    feat: add management methods for queue and exchanges

--- src/MCB.Core.Infra.CrossCutting.RabbitMq/Connection/Interfaces/IRabbitMqConnection.cs
+++ src/MCB.Core.Infra.CrossCutting.RabbitMq/Connection/Interfaces/IRabbitMqConnection.cs
@@ -5,7 +5,19 @@ namespace MCB.Core.Infra.CrossCutting.RabbitMq.Connection.Interfaces;
 public interface IRabbitMqConnection
     : IDisposable
 {
-    IBasicProperties CreateBasicProperties();
+    void OpenConnection(bool forceReopen = false);
+
+    bool CheckIfExchangeExists(string exchangeName);
     void PublishExchange(RabbitMqExchangeConfig exchangeConfig, string routingKey, IBasicProperties properties, ReadOnlyMemory<byte> message);
+
+    bool CheckIfQueueExists(string queueName);
+    QueueDeclareOk? QueueDeclare(RabbitMqQueueConfig queueConfig);
     void PublishQueue(RabbitMqQueueConfig queueConfig, IBasicProperties properties, ReadOnlyMemory<byte> message);
+    void PurgeQueue(string queueName);
+    bool DeleteQueue(string queueName, bool ifUnused = false, bool ifEmpty = false);
+    (uint messageCount, uint consumerCount)? GetQueueCounters(string queueName);
+
+    void DisconectConsumer(string consumerTag);
+
+    IBasicProperties CreateBasicProperties();
 }
